bb6328cb1f813867c400c7ef5d7f298f1b5db0b0
Sven Ebner <mail@svenebner.ch>
2025-04-28 15:18:22 +0200

    Added util file for profiling

---
+++ src/util/profiling.cpp
@@ -0,0 +1,138 @@
+/*++
+Copyright (c) 2006 Microsoft Corporation
+
+Module Name:
+
+    profiling.cpp
+
+Abstract:
+
+    Profiling support for viper toolchain.
+
+Author:
+
+    Sven Ebner (svenebner) 2025-04-22
+
+Revision History:
+
+--*/
+#include "util/profiling.h"
+#include "util/memory_manager.h"
+#include "util/stopwatch.h"
+#include "util/trace.h"
+#include<iomanip>
+#include<iostream>
+
+/**
+ * @brief Constructor for the profiling class.
+ *
+ * @param enable Boolean flag to enable or disable profiling.
+ */
+profiling::profiling(const bool enable):
+    enabled(enable)
+{}
+
+/**
+ * @brief Updates the profiling state when entering a new scope.
+ *
+ * @param scope The scope level to update.
+ */
+void profiling::scope_update(const unsigned scope){
+    if (!enabled) return;
+
+    backtracking_vector.setx(scope, stopwatch(), stopwatch());
+    backtracking_vector[scope].start();
+    const double currSeconds = mam_stopwatch.get_seconds();
+    //Trace steps made before pushing new scope
+    if ( entered_mam_loop && currSeconds >= 0.001) {
+        high_time_count++;
+        add_high_time_node(currentNode, currSeconds);
+        TRACE("profiling_cdcl",
+          tout << "node: " << currentNode << ", props in node: " << mam_loop_itrs <<
+          ", entered loop: " << entered_mam_loop << ", time: " << currSeconds << "\n";);
+
+    }
+
+    currentNode++;
+    mam_loop_itrs = 0;
+    entered_mam_loop = false;
+    mam_stopwatch.reset();
+
+}
+
+/**
+ * @brief Updates the profiling state during backtracking.
+ *
+ * @param num_scopes The number of scopes being backtracked.
+ * @param new_lvl The new scope level after backtracking.
+ */
+void profiling::backtracking_update(const unsigned num_scopes, const unsigned new_lvl) {
+    if (!enabled) return;
+
+    TRACE("profiling_cdcl",
+                  tout << "backtracking: " << num_scopes << ", new_lvl: " << new_lvl <<
+                  ", node: " << currentNode << ", time since last here: " <<
+                  backtracking_vector[new_lvl].get_seconds() << "\n";);
+    add_backtracking_node(currentNode);
+    currentNode++;
+
+}
+
+/**
+ * @brief Collects profiling statistics and updates the provided statistics object.
+ *
+ * @param st Reference to the statistics object to update.
+ */
+void profiling::collect_statistics(statistics & st) const {
+    if (!enabled) return;
+
+    st.update("high time count", high_time_count);
+    st.update("max node", currentNode);
+    high_time_backtracking_distance();
+}
+
+/**
+ * @brief Calculates and outputs the distance between high-time nodes and backtracking nodes.
+ *
+ * @param out Optional output stream to write the results. Defaults to std::cerr if null.
+ */
+void profiling::high_time_backtracking_distance(std::ostream * out) const {
+    if (!enabled || backtracking_nodes.empty()) return;
+
+    std::ostream & os = out ? *out : std::cerr;
+
+    os << "backtracking_nodes: " << backtracking_nodes.size() << ", high_time_nodes: " << high_time_nodes.size() << "\n";
+
+    unsigned prev_backtracking_node = backtracking_nodes[0];
+
+    // Edgecase
+    if (backtracking_nodes.size() == 1) {
+        for (const std::tuple high_time_tuple : high_time_nodes) {
+            const unsigned high_time_node = std::get<0>(high_time_tuple);
+            const double time = std::get<1>(high_time_tuple);
+            os << "node: " << high_time_node << ", backtrack dist: " << (high_time_node - prev_backtracking_node) << ", time: " << time << "\n";
+        }
+    }
+
+    // Next backtracking node to fetch
+    unsigned back_index = 2;
+    unsigned curr_backtracking_node = backtracking_nodes[1];
+    const unsigned vec_size = backtracking_nodes.size();
+    for (const std::tuple high_time_tuple : high_time_nodes) {
+
+        const unsigned high_time_node = std::get<0>(high_time_tuple);
+        const double time = std::get<1>(high_time_tuple);
+
+        while ( curr_backtracking_node < high_time_node && back_index < vec_size) {
+            prev_backtracking_node = curr_backtracking_node;
+            curr_backtracking_node = backtracking_nodes[back_index];
+            back_index++;
+        }
+
+        const long back_dist = static_cast<long>(high_time_node) - prev_backtracking_node;
+        const long front_dist = static_cast<long>(high_time_node) - curr_backtracking_node;
+
+        const long min_dist = std::abs(back_dist) < std::abs(front_dist) ? back_dist : front_dist;
+        os << "node: " << high_time_node << ", backtrack dist: " << min_dist << ", time: " << time << "\n";
+    }
+}
---
+++ src/util/profiling.h
@@ -0,0 +1,74 @@
+/*++
+Copyright (c) 2006 Microsoft Corporation
+
+Module Name:
+
+    profiling.h
+
+Abstract:
+
+    Profiling support for viper toolchain.
+
+Author:
+
+    Sven Ebner (svenebner) 2025-04-22
+
+Revision History:
+
+--*/
+#pragma once
+
+#include<ostream>
+#include "util/stopwatch.h"
+#include "util/vector.h"
+#include "util/statistics.h"
+
+
+
+class profiling {
+public:
+    // Bool to enable profiling
+    bool enabled{true};
+
+protected:
+    // Vector for each layer to get total time until backtrack
+    vector<stopwatch> backtracking_vector;
+    // All backtracking node numbers
+    unsigned_vector backtracking_nodes;
+    // All high time node numbers
+    vector<std::tuple<unsigned, double>> high_time_nodes;
+    // Node in smtScope CDCL tree
+    unsigned currentNode{0};
+    // Nr. of loops iterations in mam.cpp state machine
+    unsigned mam_loop_itrs{0};
+    bool entered_mam_loop{false};
+    // Timing of last mam.cpp state machine duration
+    stopwatch mam_stopwatch;
+    // Nr. of states that took longer so dominate runtime
+    unsigned high_time_count{0};
+
+public:
+    explicit profiling(bool enable);
+
+    // Update and output info for new pushed scope
+    void scope_update(unsigned scope);
+    // Trace the backtracking steps
+    void backtracking_update(unsigned num_scopes, unsigned new_lvl);
+
+    void collect_statistics(statistics& st) const;
+    // Calc min distance for each high time node to backtracking step
+    void high_time_backtracking_distance(std::ostream* out = nullptr) const;
+
+    void add_backtracking_node(const unsigned node) { backtracking_nodes.push_back(node); }
+    void add_high_time_node(const unsigned node, const double time) { high_time_nodes.push_back(std::tuple{node, time}); }
+
+    // -----------------------------------
+    //
+    // Accessors
+    //
+    // -----------------------------------
+
+    unsigned& get_mam_loop_itrs() { return mam_loop_itrs; }
+    bool& get_entered_mam_loop() { return entered_mam_loop; }
+    stopwatch& get_mam_stopwatch() { return mam_stopwatch; }
+};
